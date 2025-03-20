2839865fe751eb9354baac481a6b6600e34798f0
Anton Smirnov <tonysmn97@gmail.com>
2025-03-20 19:09:40 +0200

    Run tests on different GPU backend

--- src/NNop.jl
+++ src/NNop.jl
@@ -1,7 +1,6 @@
 module NNop
 
 using Random
-using AMDGPU
 using BFloat16s
 using GPUArrays
 using KernelAbstractions
@@ -11,11 +10,13 @@ using NNlib: ⊠, make_causal_mask, apply_attn_mask
 using StaticArrays
 using Zygote
 
+import Adapt
 import KernelAbstractions as KA
 import SIMD
 
 include("simd.jl")
-# include("softmax.jl")
+include("groupreduce.jl")
+include("softmax.jl")
 include("mma.jl")
 include("attention.jl")
 include("attention_bwd.jl")
@@ -37,15 +38,15 @@ function naive_attention(q, k, v; causal::Bool)
     return v ⊠ naive_softmax(a .- am; dims=1)
 end
 
-function test_softmax()
+function test_softmax(kab)
     for seq_len in (1024, 2048)
-        x = ROCArray(rand(Float32, seq_len, 4))
+        x = Adapt.adapt(kab, rand(Float32, seq_len, 4))
         y1 = naive_softmax(x; dims=1)
         y2 = NNop.online_softmax(x)
         @assert y1 ≈ y2
     end
 
-    x = ROCArray(rand(Float32, 8192, 1024))
+    x = Adapt.adapt(kab, rand(Float32, 8192, 1024))
     cache = GPUArrays.AllocCache()
 
     y1 = naive_softmax(x; dims=1)
@@ -53,27 +54,33 @@ function test_softmax()
     @assert y1 ≈ y2
 
     println("Naїve softmax:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache naive_softmax($x; dims=1)
+    @btime GPUArrays.@cached $cache begin
+        naive_softmax($x; dims=1)
+        KA.synchronize($kab)
+    end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
 
     println("Online softmax:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache online_softmax($x)
+    @btime GPUArrays.@cached $cache begin
+        online_softmax($x)
+        KA.synchronize($kab)
+    end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
 
     return
 end
 
-function test_flash_attention()
+function test_flash_attention(kab)
     Random.seed!(0)
     T = Float32
-    E, QL, KL, H, B = 64, 4096, 4096, 2, 1
-    causal = true
+    E, QL, KL, H, B = 64, 64, 64, 1, 1
+    causal = false
 
-    q = ROCArray(rand(T, E, QL, H, B))
-    k = ROCArray(rand(T, E, KL, H, B))
-    v = ROCArray(rand(T, E, KL, H, B))
+    q = Adapt.adapt(kab, ones(T, E, QL, H, B))
+    k = Adapt.adapt(kab, ones(T, E, KL, H, B))
+    v = Adapt.adapt(kab, ones(T, E, KL, H, B))
 
     on = naive_attention(q, k, v; causal)
     o, ms, ls = flash_attention(q, k, v; causal)
@@ -83,43 +90,51 @@ function test_flash_attention()
         sum(naive_attention(q, k, v; causal))
     end
 
-    Δ = ROCArray(ones(T, E, QL, H, B))
+    Δ = Adapt.adapt(kab, ones(T, E, QL, H, B))
 
     dq, dk, dv = ∇flash_attention(Δ, o, ms, ls, q, k, v; causal)
     @assert isapprox(dq, ∇[1]; atol=1e-3, rtol=1e-3)
     @assert isapprox(dk, ∇[2]; atol=1e-3, rtol=1e-3)
     @assert isapprox(dv, ∇[3]; atol=1e-3, rtol=1e-3)
 
-    AMDGPU.synchronize()
     GC.gc(false)
     GC.gc(true)
+    return
 
     cache = GPUArrays.AllocCache()
 
     println("Naїve attention FWD:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache naive_attention($q, $k, $v; causal=$causal)
+    @btime GPUArrays.@cached $cache begin
+        naive_attention($q, $k, $v; causal=$causal)
+        KA.synchronize($kab)
+    end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
 
     println("Flash attention FWD:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache flash_attention($q, $k, $v; causal=$causal)
+    @btime GPUArrays.@cached $cache begin
+        flash_attention($q, $k, $v; causal=$causal)
+        KA.synchronize($kab)
+    end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
 
     println("Naїve attention FWD + BWD:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache begin
+    @btime GPUArrays.@cached $cache begin
         ∇ = Zygote.gradient($q, $k, $v) do q, k, v
             sum(naive_attention(q, k, v; causal=$causal))
         end
+        KA.synchronize($kab)
     end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
 
     println("Flash attention FWD + BWD:")
-    @btime AMDGPU.@sync GPUArrays.@cached $cache begin
+    @btime GPUArrays.@cached $cache begin
         o, ms, ls = flash_attention($q, $k, $v; causal=$causal)
         sum(o)
         dq, dk, dv = ∇flash_attention($Δ, o, ms, ls, $q, $k, $v; causal=$causal)
+        KA.synchronize($kab)
     end
     println(" - Peak memory usage: $(Base.format_bytes(sizeof(cache)))")
     GPUArrays.unsafe_free!(cache)
