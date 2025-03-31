55ab20776346a0fbebb359bdcd452a43698195a2
Anton Smirnov <tonysmn97@gmail.com>
2025-04-01 00:05:57 +0300

    Add CUDA CI

---
+++ ext/NNopCUDAExt.jl
@@ -0,0 +1,11 @@
+module NNopCUDAExt
+
+using CUDA
+using NNop
+
+function NNop._shared_memory(::CUDABackend, device_id::Integer)
+    dev = CUDA.devices()[device_id]
+    return UInt64(CUDA.attribute(dev, CUDA.CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK))
+end
+
+end
